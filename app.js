cd04d5f1a708a6006b2d3f68a59fcc946ab7b038
rparias <ariasron@hotmail.com>
2021-03-06 14:12:45 -0500

    Adds notes dynamically below the form

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ class App {
         this.notes = [];
 
         this.$form = document.querySelector('#form');
+        this.$placeholder = document.querySelector('#placeholder');
+        this.$notes = document.querySelector('#notes');
         this.$noteTitle = document.querySelector('#note-title');
         this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
@@ -57,12 +59,30 @@ class App {
             id: this.generateId()
         };
         this.notes = [...this.notes, newNote];
-        console.log(this.notes);
+        this.displayNotes();
     }
 
     generateId() {
         let id = this.notes.length > 0 ? this.notes[this.notes.length -1].id + 1 : 1;
         return id;
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0;
+        this.$placeholder.style.display = !hasNotes ? 'none' : 'flex';
+
+        this.$notes.innerHTML = this.notes.map(note => `
+        <div style="background: ${note.color};" class="note">
+            <div class="${note.title && 'note-title'}">${note.title}</div>
+            <div class="note-text">${note.text}</div>
+            <div class="toolbar-container">
+            <div class="toolbar">
+                <img class="toolbar-color" src="./img/baseline_palette_black_18dp.png">
+                <img class="toolbar-delete" src="./img/baseline_delete_black_18dp.png">
+            </div>
+            </div>
+        </div>
+        `).join(" ");
     }
 }
 
